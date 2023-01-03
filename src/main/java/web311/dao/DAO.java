7843332d6be928c197b6a31b0f30e91bc94f8a7c
Roman M.E <110542682+Satturi@users.noreply.github.com>
2023-01-03 18:45:36 +0500

    Update DAO.java

--- src/main/java/web311/dao/DAO.java
+++ src/main/java/web311/dao/DAO.java
@@ -1,10 +1,10 @@
 package web311.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import web311.models.Users;
 
-import java.util.List;
-
+@Repository
 public interface DAO extends JpaRepository<Users, Integer> {
 
 }
